435841aa8ebf97cfb4688c2bae66004669181688
isaiasiotti18 <isaiasiottiprofissional@gmail.com>
2025-12-25 18:03:52 -0300

    Implement authentication flow, user schema, and video upload components

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,7 +1,10 @@
-import "dotenv/config";
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 
-const sql = neon(process.env.DATABASE_URL_POSTGRES!);
+if (!process.env.DATABASE_URL_POSTGRES) {
+  throw new Error("DATABASE_URL_POSTGRES não definida");
+}
+
+const sql = neon(process.env.DATABASE_URL_POSTGRES);
 
 export const db = drizzle(sql);
---
+++ lib/auth.ts
@@ -0,0 +1,20 @@
+import { db } from "@/drizzle/db";
+import { account, session, user, verification } from "@/drizzle/schema";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: { user, session, account, verification },
+  }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
+  plugins: [nextCookies()],
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+});
